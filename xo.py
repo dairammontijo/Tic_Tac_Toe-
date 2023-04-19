48ea5169bca0658578e1c0871acc59ff88255865
dairammontijo <dairammontijo@yahoo.com>
2023-04-18 21:03:23 -0400

    first commit

---
+++ xo.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import messagebox 
+
+root = Tk()
+root.title('')
+root.iconbitmap('')
+
+def b_click(b):
+    pass
+
+#Buttons 
+b1 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b1))
+b2 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b2))
+b3 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b3))
+
+b4 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b4))
+b5 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b5))
+b6 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b6))
+
+b7 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b7))
+b8 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b8))
+b9 = Button (root, text= "", font= ("Helvetica", 20), width=6, bg="Silver", command= lambda : b_click(b9))
+
+#Grid the Buttons
+b1.grid(row=0, column=0)
+b2.grid(row=0, column=1)
+b3.grid(row=0, column=2)
+
+b4.grid(row=1, column=0)
+b5.grid(row=1, column=1)
+b6.grid(row=1, column=2)
+
+b7.grid(row=2, column=0)
+b8.grid(row=2, column=1)
+b9.grid(row=2, column=2)
+
+root.mainloop()
