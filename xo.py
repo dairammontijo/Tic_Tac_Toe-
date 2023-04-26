7bdc2eb0ac0697fd3e7057db64e2b13ba2314933
dairammontijo <dairammontijo@yahoo.com>
2023-04-26 12:32:08 -0400

    third commit

--- xo.py
+++ xo.py
@@ -9,6 +9,146 @@ root.iconbitmap('')
 clicked = True 
 count = 0
 
+#disable_buttons
+def disable_buttons():
+    b1.config(state=DISABLED)
+    b2.config(state=DISABLED)
+    b3.config(state=DISABLED)
+    b4.config(state=DISABLED)
+    b5.config(state=DISABLED)
+    b6.config(state=DISABLED)
+    b7.config(state=DISABLED)
+    b8.config(state=DISABLED)
+    b9.config(state=DISABLED)
+
+
+
+
+#check if someone won
+def check_win():
+    global winner
+    winner = False
+
+    #check ror X
+    if b1["text"] == "X" and b2["text"] == "X" and b3["text"] =="X":
+        b1.config(bg="red")
+        b2.config(bg="red")
+        b3.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+    elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] =="X":
+        b4.config(bg="red")
+        b5.config(bg="red")
+        b6.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+    elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] =="X":
+        b7.config(bg="red")
+        b8.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+    elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] =="X":
+        b1.config(bg="red")
+        b4.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+    elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] =="X":
+        b2.config(bg="red")
+        b5.config(bg="red")
+        b8.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+    elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] =="X":
+        b3.config(bg="red")
+        b6.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+    elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] =="X":
+        b1.config(bg="red")
+        b5.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+    elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] =="X":
+        b3.config(bg="red")
+        b5.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
+        disable_buttons()
+
+#check for O
+
+    elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] =="O":
+        b1.config(bg="red")
+        b2.config(bg="red")
+        b3.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+    elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] =="O":
+        b4.config(bg="red")
+        b5.config(bg="red")
+        b6.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+    elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] =="O":
+        b7.config(bg="red")
+        b8.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+    elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] =="X":
+        b1.config(bg="red")
+        b4.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+    elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] =="O":
+        b2.config(bg="red")
+        b5.config(bg="red")
+        b8.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+    elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] =="O":
+        b3.config(bg="red")
+        b6.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+    elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] =="O":
+        b1.config(bg="red")
+        b5.config(bg="red")
+        b9.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+    elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] =="O":
+        b3.config(bg="red")
+        b5.config(bg="red")
+        b7.config(bg="red")
+        winner = True
+        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        disable_buttons()
+
+
+
+
 def b_click(b):
     global clicked, count
 
@@ -16,28 +156,28 @@ def b_click(b):
         b["text"] = "X"
         clicked = False
         count += 1 
+        check_win()
     elif b["text"] == " " and clicked == False:
         b["text"] = "O"
         clicked = True
         count += 1
+        check_win()
     else:
         messagebox.showerror("Tic Tac Toe", "Box has already been selected")
 
 
-
-
 #Buttons 
-b1 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b1))
-b2 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b2))
-b3 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b3))
+b1 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b1))
+b2 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b2))
+b3 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b3))
 
-b4 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b4))
-b5 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b5))
-b6 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b6))
+b4 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b4))
+b5 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b5))
+b6 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b6))
 
-b7 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b7))
-b8 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b8))
-b9 = Button (root, text= "", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b9))
+b7 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b7))
+b8 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b8))
+b9 = Button (root, text= " ", font= ("Helvetica", 20), width=6, height=4, bg="Silver", command= lambda : b_click(b9))
 
 #Grid the Buttons
 b1.grid(row=0, column=0)
